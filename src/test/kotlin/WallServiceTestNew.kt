b1b2ce369531cf582a0d9bb6ee125667b7575b1e
oldmurray34 <oldmurray34@gmail.com>
2021-08-20 19:40:30 +0300

    Added code and tests

---
+++ src/test/kotlin/WallServiceTestNew.kt
@@ -0,0 +1,22 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class WallServiceTestNew {
+
+    @Test
+    fun getPosts() {
+    }
+
+    @Test
+    fun setPosts() {
+    }
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun update() {
+    }
+}
